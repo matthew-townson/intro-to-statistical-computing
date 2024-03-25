6d87d00dc7e0b319abd470cf5c55760d868faec8
matthew-townson <mjwtownson@gmail.com>
2024-03-25 17:34:55 +0000

    updated CWind Direction
    better labelling

--- src/LDSWindDir.R
+++ src/LDSWindDir.R
@@ -19,11 +19,11 @@ frequencyDataFrame87$Value <- as.factor(names(freqTable87))
 # Plot
 ggplot(frequencyDataFrame87, aes(x = Value, y = Freq)) +
   geom_bar(stat = "identity", fill = "skyblue", color = "black") +
-  labs(title = "Cardinal Wind Direction Frequency (1987)",
+  labs(title = "Camborne Cardinal Wind Directions (1987)",
        x = "Direction", y = "Frequency") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("./exported/LDS/CardinalWindCamborne/CardinalWindCamborne1987.png", width = 16, height = 9, device="png", dpi=1600)
+ggsave("./exported/LDS/CardinalWindCamborne/CardinalWind1987.png", width = 16, height = 9, device="png", dpi=1600)
 
 #Doing same for camborne 2015
 freqTable15 <- table(camborne2015$Cardinal.Direction)
@@ -34,11 +34,11 @@ frequencyDataFrame15$Value <- as.factor(names(freqTable15))
 # Plot
 ggplot(frequencyDataFrame15, aes(x = Value, y = Freq)) +
   geom_bar(stat = "identity", fill = "skyblue", color = "black") +
-  labs(title = "Cardinal Wind Direction Frequency (2015)",
+  labs(title = "Camborne Cardinal Wind Directions (2015)",
        x = "Direction", y = "Frequency") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("./exported/LDS/CardinalWindCamborne/CardinalWindCamborne2015.png", width = 16, height = 9, device="png", dpi=1600)
+ggsave("./exported/LDS/CardinalWindCamborne/CardinalWind2015.png", width = 16, height = 9, device="png", dpi=1600)
 
 # Comparing years - add Dataset attribute and combine
 frequencyDataFrame87$DS <- "DS 1987"
@@ -49,8 +49,8 @@ completeDataset <- rbind(frequencyDataFrame87, frequencyDataFrame15)
 # Plot
 ggplot(completeDataset, aes(x = Value, y = Freq, fill = DS)) +
   geom_bar(stat = "identity", position = "dodge", color = "black") +
-  labs(title = "Cardinal Wind Direction Frequency (1987/2015)",
+  labs(title = "Camborne Cardinal Wind Direction (1987/2015)",
        x = "Direction", y = "Frequency") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("./exported/LDS/CardinalWindCamborne/CardinalWindCamborneCombined.png", width = 16, height = 9, device="png", dpi=1600)
+ggsave("./exported/LDS/CardinalWindCamborne/CardinalWindCombined.png", width = 16, height = 9, device="png", dpi=1600)
