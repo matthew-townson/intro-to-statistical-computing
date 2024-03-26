9920a27f3a754cfa48d8b583c984f6cd90aa9250
matthew-townson <mjwtownson@gmail.com>
2024-03-26 13:14:39 +0000

    added simple ver
    for live demo

---
+++ src/simple rnorm.R
@@ -0,0 +1,11 @@
+# Plots a normal distribution
+
+# set x bound sequence of values
+x <- seq(-5, 5, length.out = 1000)
+
+# generate pdf
+pdf <- dnorm(x, mean = 0, sd = 1)
+
+# plot
+plot(x, pdf, type = "l", col = "blue", main = "Standard Normal",
+     xlab = "n", ylab = "p")
