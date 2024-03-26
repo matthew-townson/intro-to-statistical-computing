a66df54a10cad613a2283a959f630696ba8906f5
matthew-townson <mjwtownson@gmail.com>
2024-03-26 12:49:57 +0000

    added normal example

---
+++ src/rnorm.R
@@ -0,0 +1,13 @@
+# Plots a normal distribution
+
+# set x bound sequence of values
+x <- seq(-5, 5, length.out = 1000)
+
+# generate pdf
+pdf <- dnorm(x, mean = 0, sd = 1)
+
+# plot
+png("./exported/distribution/normalexample.png", width = 1920, height = 1080)
+plot(x, pdf, type = "l", col = "blue", main = "Standard Normal",
+     xlab = "x", ylab = "p")
+dev.off()
