fd7be20e9db7ef97d97d8f75bd981fdad9dbfb10
matthew-townson <mjwtownson@gmail.com>
2024-03-26 12:08:41 +0000

    updated formatting

--- src/rbinom.R
+++ src/rbinom.R
@@ -2,9 +2,9 @@
 
 # Probability mass function with 20 elements, and a probability of success of 0.2,
 # show all probabilities for all elements for later plotting
-pmf <- dbinom(0:20, size = 20, prob=0.2)
+pmf <- dbinom(0:20, size = 20, prob = 0.2)
 
 # plot the probabilities
 png("./exported/distribution/binomexample.png", width = 1920, height = 1080)
-plot(0:20, main = "Binomial distribution", xlab = "Attempt no.", ylab = "P-value", pmf, type = 'h')
+plot(0:20, main = "Binomial distribution", xlab = "Successes", ylab = "P-value", pmf, type = "h")
 dev.off()
